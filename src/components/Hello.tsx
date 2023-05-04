c7788794f8734f1f588bac69c85f6edb6f05e50c
Jonathan B. Sabuncu <sundfo057@gmail.com>
2023-05-04 13:58:50 -0400

    changed greeting

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,7 +5,7 @@ export default function Hello ({firstName,lastName}:HelloProps) {
   return (
     <>
     <h1>
-      Good Morning⏅ {firstName} {lastName}
+      Good Afternoon⏅ {firstName} {lastName}
     </h1>
     </>
   )
